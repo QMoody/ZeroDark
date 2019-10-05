fbf33814a3f5cb2af0d0daf045318f60a3c87768
ReddyTheYeddy <45831396+ReddyTheYeddy@users.noreply.github.com>
2019-10-05 14:29:53 -0400

    Throw Orb

---
+++ ZeroDark/Assets/ZeroDark/Scripts/LightController.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightController : MonoBehaviour
+{
+    #region Variables
+    [Header("Gameobjects & Components")] // GAMEOBJECTS & COMPONENTS
+    public GameObject lightSource;
+    public GameObject lightHoldPoint;
+    public Rigidbody rbShpere;
+    public PlayerController playerCon;
+
+    public float lightFollowSpeed;
+
+    public float throwForce;
+
+    private Vector3 velocity = Vector3.zero;
+
+    public bool isHolding;
+    #endregion
+
+    //--//--//--//--//--//--//--//--//--//--//--//--//--//--//--//--//--//
+    
+    #region Initialize
+    private void Awake()
+    {
+        rbShpere = lightSource.GetComponent<Rigidbody>();
+        playerCon = GetComponent<PlayerController>();
+    }
+
+    void Start()
+    {
+        
+    }
+    #endregion
+
+    void Update()
+    {
+        if (isHolding == true)
+            HoldLight();
+
+        ThrowLight();
+    }
+
+    void HoldLight()
+    {
+        float step = Time.deltaTime;
+
+        lightSource.transform.position = Vector3.SmoothDamp(lightSource.transform.position, lightHoldPoint.transform.position, ref velocity, lightFollowSpeed);
+    }
+
+    void ThrowLight()
+    {
+        if (Input.GetButtonDown("Fire1") && isHolding == true)
+        {
+            isHolding = false;
+
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit, 50f))
+            {
+                Debug.Log(hit.point);
+            }
+
+            Vector3 direct = (hit.point - lightSource.transform.position).normalized * throwForce;
+            rbShpere.useGravity = true;
+            rbShpere.AddForce(direct, ForceMode.Impulse);
+        }
+        else if (Input.GetButtonDown("Fire2"))
+        {
+            isHolding = true;
+            rbShpere.useGravity = false;
+        }
+    }
+}
--- ZeroDark/Assets/ZeroDark/Scripts/PlayerController.cs
+++ ZeroDark/Assets/ZeroDark/Scripts/PlayerController.cs
@@ -13,11 +13,13 @@ public class PlayerController : MonoBehaviour
     [Header("Movement Variables")] // MOVEMENT VARIABLES
     public float walkAccelSpeed;
     public float walkSpeed;
-    //Vector2 currentAccelSpeed;
     private float currentForwardSpeed;
 
     public float turnSpeed;
 
+    [Header("Camera Variables")] // CAMERA VARIABLES
+    private Vector3 camDistance;
+    private Vector3 camDistanceTarget;
     #endregion
 
     //--//--//--//--//--//--//--//--//--//--//--//--//--//--//--//--//--//
@@ -30,13 +32,14 @@ public class PlayerController : MonoBehaviour
 
     void Start()
     {
-        
+        camDistanceTarget = transform.InverseTransformPoint(playerCamera.transform.position);
     }
     #endregion
 
     void Update()
     {
         StandardMovement();
+        CameraFollow();
     }
 
     #region Movement
@@ -52,18 +55,14 @@ public class PlayerController : MonoBehaviour
         Vector3 moveDir = transform.forward * -currentForwardSpeed * walkSpeed;
         rbPlayer.velocity = Vector3.MoveTowards(rbPlayer.velocity, new Vector3(moveDir.x, rbPlayer.velocity.y, moveDir.z), step * 20);
 
-        //Vector3 targetDir = new Vector3(transform.position.x + Input.GetAxisRaw("Vertical"), transform.position.y, transform.position.z);
         Vector3 targetDir = new Vector3(-Input.GetAxisRaw("Vertical"), 0, Input.GetAxisRaw("Horizontal"));
-
-        Debug.Log(targetDir);
-
         Vector3 newDir = Vector3.RotateTowards(transform.forward, targetDir, step * turnSpeed * currentForwardSpeed, 0.0f);
         transform.rotation = Quaternion.LookRotation(newDir);
+    }
 
-        //currentAccelSpeed = new Vector2(Mathf.MoveTowards(currentAccelSpeed.x, 1, step * walkAccelSpeed), Mathf.MoveTowards(currentAccelSpeed.y, 1, step * walkAccelSpeed));
-        //Vector2 inputDir = new Vector2(Input.GetAxisRaw("Vertical") * currentAccelSpeed.x, Input.GetAxisRaw("Horizontal") * currentAccelSpeed.y).normalized * walkSpeed;
-        //Vector3 moveDir = transform.forward * inputDir.x + transform.right * inputDir.y;
-        //rbPlayer.velocity = Vector3.MoveTowards(rbPlayer.velocity, new Vector3(moveDir.x, rbPlayer.velocity.y, moveDir.z), step * 20);
+    void CameraFollow()
+    {
+        playerCamera.transform.position = transform.position + camDistanceTarget;
     }
     #endregion
 }
